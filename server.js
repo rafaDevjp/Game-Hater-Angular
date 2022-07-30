46a724f3a6400397ce7ffa09cd24525ca1d41923
rafael-andrade <rafa.devjp@gmail.com>
2022-07-30 00:37:13 -0300

    correção so server.js (listen...PORT)

--- server.js
+++ server.js
@@ -2,14 +2,17 @@
 
 //
 const express = require('express');
+//
 const app = express();
+//
 const appName = 'tin-din-app';
+//
 const outPutPath = `${__dirname}/dist/${appName}`;
+//
 app.use(express.static(outPutPath));
+//
 app.get('/*', (req, res) => {
     res.sendFile(`${outPutPath}/index.html`);
 })
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT);
 
