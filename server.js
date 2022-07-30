e4f88eb5a823d215d6aef88f5752f33b995312f1
rafael-andrade <rafa.devjp@gmail.com>
2022-07-30 00:10:18 -0300

    Correção no server.js (index.html estava errado) e (outputPath: a pasta dist estava com o nome  errado)

--- server.js
+++ server.js
@@ -4,10 +4,10 @@
 const express = require('express');
 const app = express();
 const appName = 'tin-din-app';
-const outPutPath = `${__dirname}/dinst/${appName}`;
+const outPutPath = `${__dirname}/dist/${appName}`;
 app.use(express.static(outPutPath));
 app.get('/*', (req, res) => {
-    res.sendFile(`${outPutPath}/indx.html`);
+    res.sendFile(`${outPutPath}/index.html`);
 })
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
